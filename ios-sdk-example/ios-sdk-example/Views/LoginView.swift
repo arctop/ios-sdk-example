9d75735fe8150ff867cdb01cc9624329bd133711
Shai Kalev <sk@arctop.com>
2024-05-13 13:11:30 +0200

    code cleanup

--- ios-sdk-example/ios-sdk-example/Views/LoginView.swift
+++ ios-sdk-example/ios-sdk-example/Views/LoginView.swift
@@ -26,42 +26,8 @@ struct LoginView: View {
                             await viewModel.login()
                         }
                     }
-                
-//                TextField("Email", text: $email)
-//                    .autocorrectionDisabled(true)
-//                    .textInputAutocapitalization(.never)
-//                    .keyboardType(.emailAddress)
-//                
-//                    .padding(.bottom)
-//                SecureInputView(password: $password, placeHolder: "Password")
-//                    .padding(.bottom)
-//                
-//                
-//                Button("Login"){
-//                    onRegSuccess(email, password)
-//                }
-//                .disabled(isLoginButtonDisabled())
-//                .buttonStyle(SquareButtonStyle(size: 4))
-//                .padding(.bottom)
-                
             }.padding()
         }
     }
-//    private func onRegSuccess(_ email:String, _ password:String){
-//        Task{
-//            let result = await viewModel.login(email: email, password: password)
-//            switch result{
-//                case .failure(let error):
-//                viewModel.lastError = LocalizedAlertError(error , title: "Login Failed!")
-//                print("Error")
-//            case .success(_):
-//                // all good
-//                break
-//            }
-//        }
-//    }
-//    private func isLoginButtonDisabled() -> Bool{
-//        return password.isEmpty || email.isEmpty
-//    }
 }
 
